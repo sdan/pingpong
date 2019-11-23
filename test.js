80733f2dd530028b7863aa84bf4f95eb1a56c518
Surya Dantuluri <git@sdan.cc>
2019-11-22 18:34:24 -0800

    rm all .js files

--- test.js
+++
@@ -1,8 +0,0 @@
-var populateLogs = function() {
-  var logContainer = document.querySelector("#log");
-  logContainer.innerHTML = null
-
-  window.payload.forEach(log => {
-    console.log("cool")
-  });
-}
